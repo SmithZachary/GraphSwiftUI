d007200947cb86e6781a6353f5c064c6b9fe4500
SmithZachary <zachary.6.smith@gmail.com>
2021-09-09 17:45:22 -0500

    Initial Commit

---
+++ Graph2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Graph2
+//
+//  Created by Zach Smith on 9/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Graph2/Graph2App.swift
@@ -0,0 +1,17 @@
+//
+//  Graph2App.swift
+//  Graph2
+//
+//  Created by Zach Smith on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Graph2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
